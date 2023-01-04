8535f886e6bbef60a4769e76b1d51168d3667f46
nishtha981 <92522516+nishtha981@users.noreply.github.com>
2023-01-04 22:27:47 +0530

    Time: 5 ms (29.13%), Space: 6.1 MB (9.70%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.cpp
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -1,13 +1,31 @@
 class Solution {
 public:
     int reverse(int x) {
-    int ret = 0;
-        while (x != 0) {
-            if (abs(ret) > 214748364) 
-                return 0;
-            ret = ret * 10 + x % 10;
-            x /= 10;
+    int newnums = 0, flag = 0;
+    int n = 0;
+    if (x < 0)
+    {
+        flag = 1;
+        x = abs(x);
+    }
+    cout << "x is equal to " << x; 
+    while (x != 0)
+    { 
+        if (abs(newnums) > 214748364)
+        {
+            return 0;
         }
-        return ret;
+        newnums = (newnums * 10) + x%10;
+        x = x / 10;
+    }
+    if (flag == 1)
+    {
+        return (newnums*-1);
+    }
+    else
+    {
+        return (newnums);
+    }
+        
     }
 };
