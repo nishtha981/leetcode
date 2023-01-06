ff1c9fb351ac9616a65ca1a0052f90ac7ac59017
nishtha981 <92522516+nishtha981@users.noreply.github.com>
2023-01-06 23:46:55 +0530

    Time: 36 ms (75.40%), Space: 13.9 MB (11.14%) - LeetHub

--- 0771-jewels-and-stones/0771-jewels-and-stones.py
+++ 0771-jewels-and-stones/0771-jewels-and-stones.py
@@ -4,7 +4,7 @@ class Solution:
         j=[]
         j=jewels[::1]
         count=0
-        for i in range(len(stones)):
-            if(stones[i] in jewels):
+        for i in stones:
+            if(i in jewels):
                 count+=1
         return count
